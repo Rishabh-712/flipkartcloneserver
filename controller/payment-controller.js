3299ad3971a0ad8269033df69b69ffc1ccd85637
Rishabh Rawat <rishabhrawatcool712@gmail.com>
2023-07-05 01:22:54 +0530

    v4

--- controller/payment-controller.js
+++ controller/payment-controller.js
@@ -19,61 +19,63 @@ export const addPaymentGateway = async (request, response) => {
   }
 };
 
-// export const paymentResponse = (request, response) => {
-//   const form = new formidable.IncomingForm();
-//   const paytmCheckSum = request.body.CHECKSUMHASH;
-//   delete request.body.CHECKSUMHASH;
-
-//   const isVerifySignature = paytmchecksum.verifySignature(
-//     request.body,
-//     "bKMfNxPPf_QdZppa",
-//     paytmCheckSum
-//   );
-//   if (isVerifySignature) {
-//     let paytmParams = {};
-//     paytmParams["MID"] = request.body.MID;
-//     paytmParams["ORDERID"] = request.body.ORDERID;
-
-//     paytmchecksum
-//       .generateSignature(paytmParams, "bKMfNxPPf_QdZppa")
-//       .then(function (checksum) {
-//         paytmParams["CHECKSUMHASH"] = checksum;
-
-//         const post_data = JSON.stringify(paytmParams);
-
-//         const options = {
-//           hostname: "securegw-stage.paytm.in",
-//           port: 443,
-//           path: "/order/status",
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//             "Content-Length": post_data.length,
-//           },
-//         };
-
-//         let res = "";
-//         const post_req = https.request(options, function (post_res) {
-//           post_res.on("data", function (chunk) {
-//             res += chunk;
-//           });
-
-//           post_res.on("end", function () {
-//             let result = JSON.parse(res);
-//             console.log(result);
-//             response.redirect(`http://localhost:3000/`);
-//           });
-//         });
-//         post_req.write(post_data);
-//         post_req.end();
-//       });
-//   } else {
-//     console.log("Checksum Mismatched");
-//   }
-// };
-
 export const paymentResponse = (request, response) => {
-  response.redirect(
-    `https://64a46c0b5d78460d2fa6dae6--super-malasada-d02972.netlify.app/`
+  const form = new formidable.IncomingForm();
+  const paytmCheckSum = request.body.CHECKSUMHASH;
+  delete request.body.CHECKSUMHASH;
+
+  const isVerifySignature = paytmchecksum.verifySignature(
+    request.body,
+    "bKMfNxPPf_QdZppa",
+    paytmCheckSum
   );
+  if (isVerifySignature) {
+    let paytmParams = {};
+    paytmParams["MID"] = request.body.MID;
+    paytmParams["ORDERID"] = request.body.ORDERID;
+
+    paytmchecksum
+      .generateSignature(paytmParams, "bKMfNxPPf_QdZppa")
+      .then(function (checksum) {
+        paytmParams["CHECKSUMHASH"] = checksum;
+
+        const post_data = JSON.stringify(paytmParams);
+
+        const options = {
+          hostname: "securegw-stage.paytm.in",
+          port: 443,
+          path: "/order/status",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Content-Length": post_data.length,
+          },
+        };
+
+        let res = "";
+        const post_req = https.request(options, function (post_res) {
+          post_res.on("data", function (chunk) {
+            res += chunk;
+          });
+
+          post_res.on("end", function () {
+            let result = JSON.parse(res);
+            console.log(result);
+            response.redirect(
+              `https://64a46c0b5d78460d2fa6dae6--super-malasada-d02972.netlify.app/`
+            );
+          });
+        });
+        post_req.write(post_data);
+        post_req.end();
+      });
+  } else {
+    console.log("Checksum Mismatched");
+  }
 };
+
+// export const paymentResponse = (request, response) => {
+//   response.redirect(
+//     `https://64a46c0b5d78460d2fa6dae6--super-malasada-d02972.netlify.app/`
+//   );
+// };
